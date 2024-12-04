2a8b287d1f677ed3dfd048b4ee5c013e449f919a
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-04 19:44:22 -0300

    CREATE API inside accounts microservice - Part 1

--- accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
@@ -1,16 +1,25 @@
 package com.easybytes.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.easybytes.accounts.constants.AccountsConstants;
+import com.easybytes.accounts.dto.CustomerDto;
+import com.easybytes.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping(path = "/api/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hi World!";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
 }
---
+++ accounts/src/main/java/com/easybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.easybytes.accounts.service.impl;
+
+import com.easybytes.accounts.dto.CustomerDto;
+import com.easybytes.accounts.repository.AccountsRepository;
+import com.easybytes.accounts.repository.CustomerRepository;
+import com.easybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AccountsServiceImpl implements IAccountsService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+
+    }
+
+}
