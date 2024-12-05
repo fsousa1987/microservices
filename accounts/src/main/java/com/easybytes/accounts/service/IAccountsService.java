604e2faada9cea0c3f128561566330025a72e6e6
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-05 13:54:07 -0300

    UPDATE API inside accounts microservice

--- accounts/src/main/java/com/easybytes/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/easybytes/accounts/service/IAccountsService.java
@@ -15,4 +15,10 @@ public interface IAccountsService {
      */
     CustomerDto fetchAccount(String mobileNumber);
 
+    /**
+     * @param customerDto - CustomerDto Object
+     * @return boolean indicating if the update of Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
 }
